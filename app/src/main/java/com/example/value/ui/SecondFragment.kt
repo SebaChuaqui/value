0f73ff059889d15a9bc647826e7b2e2296ead119
SebaChuaqui <67081436+SebaChuaqui@users.noreply.github.com>
2020-12-05 18:24:06 -0300

    Intento 4

--- app/src/main/java/com/example/value/ui/SecondFragment.kt
+++ app/src/main/java/com/example/value/ui/SecondFragment.kt
@@ -1,22 +1,37 @@
 package com.example.value.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.value.MyViewModel.ValueViewModel
 import com.example.value.R
+import kotlinx.android.synthetic.main.fragment_second.*
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
 
+
+    lateinit var  mValueViewModel: ValueViewModel
+    private var idDato: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mValueViewModel = ViewModelProvider(this).get(ValueViewModel::class.java)
+        arguments?.let {
+            idDato = it.getString("codigo")
+            Log.d("OBJ", idDato.toString())
+        }
+    }
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
@@ -25,8 +40,22 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        idDato?.let {
+            mValueViewModel.getOneCode(it).observe(viewLifecycleOwner, Observer {
+                Log.d("OBJ_LIV", it.codigo)
+                code.setText(it.codigo)
+                Log.d("OBJ_LIV", it.valor)
+                value.setText(it.valor)
+            })
+
+        }
+
+        button.setOnClickListener{
+
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+
     }
+
 }
